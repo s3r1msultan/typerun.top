73a59129948cb90f17722c6e24bce97e693dfaa1
s3r1msultan <hamlethan1999@gmail.com>
2024-02-09 05:18:26 +0600

    changed README file and added reminder to do two kind of apps

--- src/App.tsx
+++ src/App.tsx
@@ -3,30 +3,45 @@ import "./App.css";
 
 function App() {
   const [input, setInput] = useState("");
-  const [words, setWords] = useState("I want something just like this");
-  const [leftWords, setLeftWords] = useState(words);
-  const [typedWords, setTypedWords] = useState("");
+  const [words, setWords] = useState("I need somebody who can love me at my worst");
+  const [leftLetters, setLeftLetters] = useState(words);
+  const [wrongLetters, setWrongLetters] = useState("");
+  const [typedLetters, setTypedLetters] = useState("");
 
-  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
-    const currentValue = event.target.value;
+  function handleInputAdd(event: React.ChangeEvent<HTMLInputElement>): void {
+    event.preventDefault();
+    const currentValue = event.currentTarget.value;
     setInput(currentValue);
-    if (leftWords.charAt(0) === currentValue.charAt(currentValue.length - 1)) {
-      setTypedWords((prev) => prev + leftWords.charAt(0));
-      setLeftWords((prev) => prev.slice(1, prev.length));
+    if (leftLetters.charAt(0) === currentValue.charAt(currentValue.length - 1)) {
+      setTypedLetters((prev) => prev + leftLetters.charAt(0));
+      setLeftLetters((prev) => prev.slice(1, prev.length));
+    } else {
+      setWrongLetters((prev) => prev + leftLetters.charAt(0));
+      setLeftLetters((prev) => prev.slice(1, prev.length));
     }
   }
+
+  function handleInputDelete(event: React.KeyboardEvent<HTMLInputElement>): void {
+    if (event.key === "Backspace" && typedLetters.length === input.length) {
+      setLeftLetters((prev) => typedLetters.charAt(typedLetters.length - 1) + prev);
+      setTypedLetters((prev) => prev.slice(0, prev.length - 1));
+    }
+  }
+
   return (
     <div className="bg-gray-300">
       <p>
-        <span className="text-green-600">{typedWords}</span>
-        <span className="text-blue-600">{leftWords}</span>
+        <span className="text-green-500">{typedLetters}</span>
+        <span className="text-red-600">{wrongLetters}</span>
+        <span className="text-blue-600">{leftLetters}</span>
       </p>
       <input
         className="text-red-700 w-full text-center bg-transparent outline-none"
         type="text"
         placeholder="Enter something"
         value={input}
-        onChange={handleInputChange}
+        onChange={handleInputAdd}
+        onKeyDown={handleInputDelete}
       />
       <p>{input}</p>
     </div>
