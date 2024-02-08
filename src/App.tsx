2b5b7c724d36489736444f0d959d091867542fb7
s3r1msultan <hamlethan1999@gmail.com>
2024-02-09 03:09:45 +0600

    fixed wrong work of typing and comparison of typed text and initial one

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,25 @@
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
   const [input, setInput] = useState("");
-  const words = useRef<HTMLElement | null>(null);
-  const typedWords = useRef<HTMLElement | null>(null);
+  const [words, setWords] = useState("I want something just like this");
+  const [leftWords, setLeftWords] = useState(words);
+  const [typedWords, setTypedWords] = useState("");
 
   function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
-    event.preventDefault();
-    setInput(event.currentTarget.value);
-    if (
-      words.current &&
-      typedWords.current &&
-      words.current.textContent?.includes(typedWords.current?.textContent || "")
-    ) {
-      typedWords.current.textContent = event.currentTarget.value;
-      console;
-      words.current.textContent = words.current.textContent.slice(
-        typedWords.current.textContent.length,
-        -1
-      );
+    const currentValue = event.target.value;
+    setInput(currentValue);
+    if (leftWords.charAt(0) === currentValue.charAt(currentValue.length - 1)) {
+      setTypedWords((prev) => prev + leftWords.charAt(0));
+      setLeftWords((prev) => prev.slice(1, prev.length));
     }
   }
   return (
     <div className="bg-gray-300">
       <p>
-        <span
-          className="text-green-600"
-          ref={typedWords}
-        ></span>
-        <span
-          className="text-blue-600"
-          ref={words}
-        >
-          I want something just like this
-        </span>
+        <span className="text-green-600">{typedWords}</span>
+        <span className="text-blue-600">{leftWords}</span>
       </p>
       <input
         className="text-red-700 w-full text-center bg-transparent outline-none"
